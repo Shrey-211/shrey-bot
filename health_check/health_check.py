b07c13c7cd195b7b5d0cdfe95b886b68e47e22b9
Shreyas Jadhav <shreyas.jadhav@flytbase>
2025-02-17 19:39:15 +0530

    feat: health check added

---
+++ health_check/health_check.py
@@ -0,0 +1,33 @@
+import json
+import requests
+import yaml
+
+class OllamaClient:
+    def __init__(self):
+        with open('health_check/ollama_config.yml', 'r') as file:
+            data = yaml.safe_load(file)
+        self.chat_endpoint = data['OLLAMA_BASE']
+        self.headers = {"Content-Type": "application/json"}
+
+    def healthCheck(self):
+        try:
+            with requests.get(self.chat_endpoint, headers=self.headers) as response:
+                status = response.raise_for_status()
+                if(response.text == None):
+                    # print("Error: Health check failed with status", status)
+                    # print("Response:", response.text)
+                    return False
+                else:
+                    # print("Health check passed")
+                    return True
+        except requests.Timeout:
+            # print("Error: Request timed out")
+            return False
+
+# Testing purpose only
+def main():
+    client = OllamaClient()
+    client.healthCheck()
+
+if __name__ == "__main__":
+    main()
